b5b17e5a438446ea6b7c8cd36b95819b3b5cf402
Gunjan Rathore <rathoregunjan46@gmail.com>
2025-02-26 20:05:01 +0530

    Install Shadcn ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <h1 className="text-3xl font-bold underline text-red-300">
       Hello world!
+      <Button>Click me</Button>
     </h1>
     </div>
   )
