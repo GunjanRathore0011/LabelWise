37f9c5851c216dfcf68956a85e0792e4fb8d1bb6
Gunjan Rathore <rathoregunjan46@gmail.com>
2025-02-27 13:27:42 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,18 @@
-const express = require('express');
+const express=require("express");
 const app=express();
+require("dotenv").config();
+const dbconnect=require("./config/database");
+const cookieParser=require("cookie-parser");
 
-app.listen(3000,()=>console.log('Server is running on port 3000'));
+// port number
+const PORT=process.env.PORT;
+// connect with database;
+dbconnect();
 
+// add middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
